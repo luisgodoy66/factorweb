835408d45257f70d158a329dd38ed2e74f7a330e
luisgodoy66 <120328788+luisgodoy66@users.noreply.github.com>
2023-11-14 20:07:58 -0500

    movimiento contable en generación de egresos

--- contabilidad/views.py
+++ contabilidad/views.py
@@ -1567,7 +1567,10 @@ def GenerarEgresoDiario(request):
     # valor null
     if pid_factura =='None':
         pid_factura= 'NULL'
-        
+    # LA CUENTA de pago es null cuando es movimiento contable
+    if not pid_cuentapago:
+        pid_cuentapago='NULL'
+
     resultado=enviarPost("CALL uspGenerarEgresoContabilidad( '{0}',{1},'{2}','{3}'\
                          ,{4},'{5}',{6},'{7}','{8}'\
                          ,{9},{10},{11},'',0)"
