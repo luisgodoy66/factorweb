77c7e094fe28adac7ffd418ab764eda98aa8fef3
luisgodoy66 <luisgodoy@codigobambu.com>
2025-04-09 11:26:31 -0500

    feat(urls.py): serve media files by adding static URL configuration

--- factorweb25/urls.py
+++ factorweb25/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',include(('bases.urls','bases'),namespace='bases')),
     path('clientes/',include(('clientes.urls','clientes'),namespace='clientes')),
@@ -30,4 +31,4 @@ urlpatterns = [
     path('api/', include(('api.urls','api'),namespace='api')),
     
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
