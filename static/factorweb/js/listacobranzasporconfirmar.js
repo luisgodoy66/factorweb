f0d367c3cacdc3c6ac37e385a8730d2e25d3c3e6
luisgodoy66 <luisgodoy@codigobambu.com>
2025-04-09 12:36:46 -0500

    fix(templates): format monetary values in 'datosliquidacioncobranzas_form.html' and update ConfirmarCobranza function parameters

--- static/factorweb/js/listacobranzasporconfirmar.js
+++ static/factorweb/js/listacobranzasporconfirmar.js
@@ -1,5 +1,5 @@
-function ConfirmarCobranza(cobranza_id, tipo_operacion){
-    MensajeConfirmacion("Confirmar la cobranza " +  cobranza_id +"?",function(){
+function ConfirmarCobranza(codigo_cobranza, cobranza_id, tipo_operacion){
+    MensajeConfirmacion("Confirmar la cobranza " +  codigo_cobranza +"?",function(){
       fetchProcesar("/cobranzas/confirmarcobranza/"+cobranza_id+"/"+tipo_operacion, function(){
             location.reload();
           })
